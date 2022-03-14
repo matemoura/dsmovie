4fa4d319039d2eabbe3b56853a19bae476001a4b
Mateus Moura <mateus51moura@gmail.com>
2022-03-14 01:11:19 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,15 +1,22 @@
 import axios from "axios";
+import { useState, useEffect } from "react";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination/indes";
 import { BASE_URL } from "utils/requests";
+import { MoviePage } from "types/movie"
 
 function Listing() {
 
-    //FORMA ERRADA
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=0`)
         .then(response => {
-            console.log(response.data);
+            const data = response.data as MoviePage;
+            console.log(data);
+            setPageNumber(data.number);
         });
+    }, []);
 
     return (
         <>
